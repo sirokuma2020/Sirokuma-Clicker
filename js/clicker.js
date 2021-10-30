c40744a22b5925afe5b866d0cf30f9f921e0ab47
nikachu2012 <mindgamer2012@gmail.com>
2021-10-30 20:19:09 +0900

    update

--- js/clicker.js
+++ js/clicker.js
@@ -15,16 +15,6 @@ function pushSirokuma() { // しろくまが押されたとき
     updateHyouji();
 }
 
-function pushClicker() { // タイトルの「クリッカー」が押されたとき
-    sirokumapoint = sirokumapoint + 100000; /* 100,000 */
-    updateHyouji();
-}
-
-function pushSirokumaTitle() { // タイトルの「しろくま」が押されたとき
-    sirokumapoint = sirokumapoint + 1000000000; /* 1,000,000,000 */
-    updateHyouji();
-}
-
 function updateHyouji(){ // 表示のアップデート用
     document.getElementById('pointHyouji').innerHTML = sirokumapoint + "&nbsp しろくま";
     console.log("表示を更新しました！しろくま数は" + sirokumapoint + "です")
