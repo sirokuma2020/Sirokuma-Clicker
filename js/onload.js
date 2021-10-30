f8c35074cf11da49d176307909b03c1635e6989d
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2021-10-30 12:12:57 +0900

    Update onload.js

--- js/onload.js
+++ js/onload.js
@@ -2,6 +2,6 @@ window.onload = onLoad;
 
 function onLoad() {
     console.log("%cSirokumaClicker", 'color: black; font-size: xx-large; text-shadow:2px 0 5px #00ff00;')
-    console.log("Version 1.0.0(beta)  Bulid 1001\nCopyright (C) Nikachu All rights reserved.")
+    console.log("Version 1.0.0  Bulid 1001\nCopyright (C) Nikachu All rights reserved.")
     console.log("最大値は9007199254740991[2^53]です")
 }
